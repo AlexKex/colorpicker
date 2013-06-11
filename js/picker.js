a78a273e4f434d30ff1594526c295d3cb9a33220
Alexander <alejandro.pryakhin@gmail.com>
2013-06-11 22:41:03 +0400

    Commit alpha

---
+++ js/picker.js
@@ -0,0 +1,12 @@
+/* Colorpicker by KeX */
+/* v.0.1alpha */
+/* started 11.06.2013 */
+
+function Colorpicker(){
+    this.color = "431ec4"; // текущий цвет
+    this.div = 'body'; // куда цепляем пикер
+}
+
+Colorpicker.prototype.init(){
+    console.log(1);
+}
